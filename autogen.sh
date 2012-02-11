b8906ba2cc7710a6ff791cab488b501b6ce6f6f3
schpuntik <schpuntik@freenet.de>
2012-02-11 12:25:06 +0100

    small fix

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-autoreconf --install
-
-echo "now, execute ./configure"
-echo "after that, you could build an ipkg with 'make ipkg' in any of the"
-echo "directories which support the ipkg target."
+autoreconf -i
