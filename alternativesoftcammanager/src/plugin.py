176a0bbd257a0329ce8c4a00d361ee0f052fcea6
Taapat <taapat@gmail.com>
2012-12-05 20:43:48 +0200

    Alternative SoftCam Manager code optimization

--- alternativesoftcammanager/src/plugin.py
+++ alternativesoftcammanager/src/plugin.py
@@ -2,81 +2,102 @@
 #code and desing examples from 2boom plugins:
 #		Easy Pnel for Pli http://gisclub.tv/index.php?topic=5075.0
 #		newcamd.list switcher http://gisclub.tv/index.php?topic=4772.0
-#openpli SoftcamSetup http://openpli.git.sourceforge.net/git/gitweb.cgi?p=openpli/enigma2-plugins;a=tree;f=PLi/SoftcamSetup/
+#openpli SoftcamSetup 
+
 from Components.ActionMap import ActionMap
-from Components.config import config, ConfigSubsection, ConfigText, getConfigListEntry
+from Components.config import config, ConfigSubsection, ConfigText, \
+	getConfigListEntry
 from Components.Console import Console
 from Components.ConfigList import ConfigListScreen
 from Components.Label import Label
 from Components.Sources.List import List
 from Components.ScrollLabel import ScrollLabel
+from Plugins.Plugin import PluginDescriptor
 from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 from Tools.LoadPixmap import LoadPixmap
-from Plugins.Plugin import PluginDescriptor
+
 from enigma import eTimer
-import os
+from os import path, remove 
 
 config.plugins.AltSoftcam = ConfigSubsection()
 config.plugins.AltSoftcam.actcam = ConfigText(default = "none")
-config.plugins.AltSoftcam.camconfig = ConfigText(default = "/var/keys", visible_width = 100, fixed_size = False)
-config.plugins.AltSoftcam.camdir = ConfigText(default = "/usr/bin/cam", visible_width = 100, fixed_size = False)
+config.plugins.AltSoftcam.camconfig = ConfigText(default = "/var/keys",
+	visible_width = 100, fixed_size = False)
+config.plugins.AltSoftcam.camdir = ConfigText(default = "/usr/bin/cam",
+	visible_width = 100, fixed_size = False)
 AltSoftcamConfigError = False
-if not os.path.isdir(config.plugins.AltSoftcam.camconfig.value):
+if not path.isdir(config.plugins.AltSoftcam.camconfig.value):
 	config.plugins.AltSoftcam.camconfig.value = "none"
 	AltSoftcamConfigError = True
-if not os.path.isdir(config.plugins.AltSoftcam.camdir.value):
+if not path.isdir(config.plugins.AltSoftcam.camdir.value):
 	config.plugins.AltSoftcam.camdir.value = "none"
 	AltSoftcamConfigError = True
 
 def getcamcmd(cam):
-	if cam.lower().find("oscam") != -1:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam + " -bc " + config.plugins.AltSoftcam.camconfig.value + "/"
-	elif cam.lower().find("wicard") != -1:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam + " -d -c " + config.plugins.AltSoftcam.camconfig.value + "/wicardd.conf"
-	elif cam.lower().find("camd3") != -1:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam + " " + config.plugins.AltSoftcam.camconfig.value + "/camd3.config"
-	elif cam.lower().find("mbox") != -1:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam + " " + config.plugins.AltSoftcam.camconfig.value + "/mbox.cfg"
-	elif cam.lower().find("mpcs") != -1:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam + " -c " + config.plugins.AltSoftcam.camconfig.value
-	elif cam.lower().find("newcs") != -1:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam + " -C " + config.plugins.AltSoftcam.camconfig.value + "/newcs.conf"
-	elif cam.lower().find("vizcam") != -1:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam + " -b -c " + config.plugins.AltSoftcam.camconfig.value + "/"
-	elif cam.lower().find("rucam") != -1:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam + " -b"
+	cam = cam.lower()
+	if "oscam" in cam:
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam + " -bc " + \
+			config.plugins.AltSoftcam.camconfig.value + "/"
+	elif "wicard" in cam:
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam + " -d -c " + \
+			config.plugins.AltSoftcam.camconfig.value + "/wicardd.conf"
+	elif "camd3" in cam:
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam + " " + \
+			config.plugins.AltSoftcam.camconfig.value + "/camd3.config"
+	elif "mbox" in cam:
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam + " " + \
+			config.plugins.AltSoftcam.camconfig.value + "/mbox.cfg"
+	elif "mpcs" in cam:
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam + " -c " + \
+			config.plugins.AltSoftcam.camconfig.value
+	elif "newcs" in cam:
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam + " -C " + \
+			config.plugins.AltSoftcam.camconfig.value + "/newcs.conf"
+	elif "vizcam" in cam:
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam + " -b -c " + \
+			config.plugins.AltSoftcam.camconfig.value + "/"
+	elif "rucam" in cam:
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam + " -b"
 	else:
-		result = config.plugins.AltSoftcam.camdir.value + "/" + cam
-	return result
+		return config.plugins.AltSoftcam.camdir.value + "/" + cam
 
 class AltCamManager(Screen):
 	skin = """
 <screen position="center,center" size="630,370" title="SoftCam manager">
 	<eLabel position="5,0" size="620,2" backgroundColor="#aaaaaa" />
-<widget source="list" render="Listbox" position="10,15" size="340,300" scrollbarMode="showOnDemand">
+<widget source="list" render="Listbox" position="10,15" size="340,300" \
+	scrollbarMode="showOnDemand">
 	<convert type="TemplatedMultiContent">
 		{"template": [
 			MultiContentEntryPixmapAlphaTest(pos = (5, 5), size = (51, 40), png = 1), 
-			MultiContentEntryText(pos = (65, 10), size = (275, 40), font=0, flags = RT_HALIGN_LEFT, text = 0), 
-			MultiContentEntryText(pos = (5, 25), size = (51, 16), font=1, flags = RT_HALIGN_CENTER, text = 2), 
+			MultiContentEntryText(pos = (65, 10), size = (275, 40), font=0, \
+				flags = RT_HALIGN_LEFT, text = 0), 
+			MultiContentEntryText(pos = (5, 25), size = (51, 16), font=1, \
+				flags = RT_HALIGN_CENTER, text = 2), 
 				],
 	"fonts": [gFont("Regular", 26),gFont("Regular", 12)],
 	"itemHeight": 50
 	}
 	</convert>
 	</widget>
-	<eLabel halign="center" position="390,10" size="210,35" font="Regular;20" text="Ecm info" transparent="1" />
-	<widget name="status" position="360,50" size="320,300" font="Regular;16" halign="left" noWrap="1" />
+	<eLabel halign="center" position="390,10" size="210,35" font="Regular;20" \
+		text="Ecm info" transparent="1" />
+	<widget name="status" position="360,50" size="320,300" font="Regular;16" \
+		halign="left" noWrap="1" />
 	<eLabel position="12,358" size="148,2" backgroundColor="#00ff2525" />
 	<eLabel position="165,358" size="148,2" backgroundColor="#00389416" />
 	<eLabel position="318,358" size="148,2" backgroundColor="#00baa329" />
 	<eLabel position="471,358" size="148,2" backgroundColor="#006565ff" />
-	<widget name="key_red" position="12,328" zPosition="2" size="148,30" valign="center" halign="center" font="Regular;22" transparent="1" />
-	<widget name="key_green" position="165,328" zPosition="2" size="148,30" valign="center" halign="center" font="Regular;22" transparent="1" />
-	<widget name="key_yellow" position="318,328" zPosition="2" size="148,30" valign="center" halign="center" font="Regular;22" transparent="1" />
-	<widget name="key_blue" position="471,328" zPosition="2" size="148,30" valign="center" halign="center" font="Regular;22" transparent="1" />
+	<widget name="key_red" position="12,328" zPosition="2" size="148,30" \
+		valign="center" halign="center" font="Regular;22" transparent="1" />
+	<widget name="key_green" position="165,328" zPosition="2" size="148,30" \
+		valign="center" halign="center" font="Regular;22" transparent="1" />
+	<widget name="key_yellow" position="318,328" zPosition="2" size="148,30" \
+		valign="center" halign="center" font="Regular;22" transparent="1" />
+	<widget name="key_blue" position="471,328" zPosition="2" size="148,30" \
+		valign="center" halign="center" font="Regular;22" transparent="1" />
 </screen>"""
 
 	def __init__(self, session):
@@ -93,7 +114,7 @@ class AltCamManager(Screen):
 				"green": self.start,
 				"red": self.stop,
 				"yellow": self.restart,
-				"blue": self.setup,
+				"blue": self.setup
 			}, -1)
 		self["status"] = ScrollLabel()
 		self["list"] = List([])
@@ -105,7 +126,8 @@ class AltCamManager(Screen):
 		self.Timer.start(1000*4, False)
 
 	def CreateInfo(self):
-		if AltSoftcamConfigError is False:
+		global AltSoftcamConfigError
+		if not AltSoftcamConfigError:
 			self.StartCreateCamlist()
 			self.listecminfo()
 
@@ -126,12 +148,14 @@ class AltCamManager(Screen):
 			self["status"].setText("")
 
 	def StartCreateCamlist(self):
-		self.Console.ePopen("ls %s" % config.plugins.AltSoftcam.camdir.value, self.CamListStart)
+		self.Console.ePopen("ls %s" % config.plugins.AltSoftcam.camdir.value,
+			self.CamListStart)
 
 	def CamListStart(self, result, retval, extra_args):
 		if not result.startswith('ls: '):
 			self.softcamlist = result
-			self.Console.ePopen("chmod 755 %s/*" % config.plugins.AltSoftcam.camdir.value)
+			self.Console.ePopen("chmod 755 %s/*" %
+				config.plugins.AltSoftcam.camdir.value)
 			self.Console.ePopen("pidof %s" % self.actcam, self.CamActive)
 
 	def CamActive(self, result, retval, extra_args):
@@ -157,12 +181,16 @@ class AltCamManager(Screen):
 			self.actcam = "none"
 		if self.actcam != "none":
 			try:
-				softpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/AlternativeSoftCamManager/images/actcam.png"))
+				softpng = LoadPixmap(cached=True,
+					path=resolveFilename(SCOPE_PLUGINS,
+					"Extensions/AlternativeSoftCamManager/images/actcam.png"))
 				self.list.append((self.actcam, softpng, self.checkcam(self.actcam)))
 			except:
 				pass
 		try:
-			softpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_PLUGINS, "Extensions/AlternativeSoftCamManager/images/defcam.png"))
+			softpng = LoadPixmap(cached=True,
+				path=resolveFilename(SCOPE_PLUGINS,
+				"Extensions/AlternativeSoftCamManager/images/defcam.png"))
 			for line in self.softcamlist.splitlines():
 				if line != self.actcam:
 					self.list.append((line, softpng, self.checkcam(line)))
@@ -171,43 +199,43 @@ class AltCamManager(Screen):
 		self["list"].setList(self.list)
 
 	def checkcam (self, cam):
-		if cam.lower().find("oscam") != -1:
-			camtext = "Oscam"
-		elif cam.lower().find("mgcamd") != -1:
-			camtext = "Mgcamd"
-		elif cam.lower().find("wicard") != -1:
-			camtext = "Wicard"
-		elif cam.lower().find("camd3") != -1:
+		cam = cam.lower()
+		if "oscam" in cam:
+			return "Oscam"
+		elif "mgcamd" in cam:
+			return "Mgcamd"
+		elif "wicard" in cam:
+			return "Wicard"
+		elif "camd3" in cam:
 			camtext = "Camd3"
-		elif cam.lower().find("mcas") != -1:
-			camtext = "Mcas"
-		elif cam.lower().find("cccam") != -1:
-			camtext = "CCcam"
-		elif cam.lower().find("gbox") != -1:
-			camtext = "Gbox"
-		elif cam.lower().find("ufs910camd") != -1:
-			camtext = "Ufs910"
-		elif cam.lower().find("incubuscamd") != -1:
-			camtext = "Incubus"
-		elif cam.lower().find("mpcs") != -1:
-			camtext = "Mpcs"
-		elif cam.lower().find("mbox") != -1:
-			camtext = "Mbox"
-		elif cam.lower().find("newcs") != -1:
-			camtext = "Newcs"
-		elif cam.lower().find("vizcam") != -1:
-			camtext = "Vizcam"
-		elif cam.lower().find("sh4cam") != -1:
-			camtext = "Sh4CAM"
-		elif cam.lower().find("rucam") != -1:
-			camtext = "Rucam"
+		elif "mcas" in cam:
+			return "Mcas"
+		elif "cccam" in cam:
+			return "CCcam"
+		elif "gbox" in cam:
+			return "Gbox"
+		elif "ufs910camd" in cam:
+			return "Ufs910"
+		elif "incubuscamd" in cam:
+			return "Incubus"
+		elif "mpcs" in cam:
+			return "Mpcs"
+		elif "mbox" in cam:
+			return "Mbox"
+		elif "newcs" in cam:
+			return "Newcs"
+		elif "vizcam" in cam:
+			return "Vizcam"
+		elif "sh4cam" in cam:
+			return "Sh4CAM"
+		elif "rucam" in cam:
+			return "Rucam"
 		else:
-			camtext = cam[0:6]
-		return camtext
+			return cam[0:6]
 
 	def start(self):
 		global AltSoftcamConfigError
-		if AltSoftcamConfigError is False:
+		if not AltSoftcamConfigError:
 			self.camstart = self["list"].getCurrent()[0]
 			if self.camstart != self.actcam:
 				print "[Alternative SoftCam Manager] Start SoftCam"
@@ -223,7 +251,7 @@ class AltCamManager(Screen):
 			self.Console.ePopen("killall -9 %s" % self.actcam)
 			print "[Alternative SoftCam Manager] stop ", self.actcam
 			try:
-				os.remove("/tmp/ecm.info")
+				remove("/tmp/ecm.info")
 			except:
 				pass
 			msg  = _("Stopping %s") % self.actcam
@@ -240,7 +268,7 @@ class AltCamManager(Screen):
 
 	def restart(self):
 		global AltSoftcamConfigError
-		if AltSoftcamConfigError is False:
+		if not AltSoftcamConfigError:
 			print "[Alternative SoftCam Manager] restart SoftCam"
 			self.camstart = self.actcam
 			if self.camstartcmd == "":
@@ -256,7 +284,7 @@ class AltCamManager(Screen):
 		self.Console.ePopen("killall -9 %s" % self.actcam)
 		print "[Alternative SoftCam Manager] stopping ", self.actcam
 		try:
-			os.remove("/tmp/ecm.info")
+			remove("/tmp/ecm.info")
 		except:
 			pass
 		self.actcam = self.camstart
@@ -294,13 +322,17 @@ class AltCamManager(Screen):
 
 class ConfigEdit(Screen, ConfigListScreen):
 	skin = """
-<screen name="ConfigEdit" position="center,center" size="500,200" title="SoftCam path configuration">
+<screen name="ConfigEdit" position="center,center" size="500,200" \
+	title="SoftCam path configuration">
 	<eLabel position="5,0" size="490,2" backgroundColor="#aaaaaa" />
-<widget name="config" position="30,20" size="460,50" zPosition="1" scrollbarMode="showOnDemand" />
+<widget name="config" position="30,20" size="460,50" zPosition="1" \
+	scrollbarMode="showOnDemand" />
 	<eLabel position="85,180" size="166,2" backgroundColor="#00ff2525" />
 	<eLabel position="255,180" size="166,2" backgroundColor="#00389416" />
-	<widget name="key_red" position="85,150" zPosition="2" size="170,30" valign="center" halign="center" font="Regular;22" transparent="1" />
-	<widget name="key_green" position="255,150" zPosition="2" size="170,30" valign="center" halign="center" font="Regular;22" transparent="1" />
+	<widget name="key_red" position="85,150" zPosition="2" size="170,30" \
+		valign="center" halign="center" font="Regular;22" transparent="1" />
+	<widget name="key_green" position="255,150" zPosition="2" size="170,30" \
+		valign="center" halign="center" font="Regular;22" transparent="1" />
 </screen>"""
 
 	def __init__(self, session):
@@ -318,14 +350,18 @@ class ConfigEdit(Screen, ConfigListScreen):
 		self.camconfigold = config.plugins.AltSoftcam.camconfig.value
 		self.camdirold = config.plugins.AltSoftcam.camdir.value
 		self.list = []
-		self.list.append(getConfigListEntry(_("SoftCam config directory"), config.plugins.AltSoftcam.camconfig))
-		self.list.append(getConfigListEntry(_("SoftCam directory"), config.plugins.AltSoftcam.camdir))
+		self.list.append(getConfigListEntry(_("SoftCam config directory"),
+			config.plugins.AltSoftcam.camconfig))
+		self.list.append(getConfigListEntry(_("SoftCam directory"),
+			config.plugins.AltSoftcam.camdir))
 		self["config"].list = self.list
 
 	def ok(self):
-		if self.camconfigold != config.plugins.AltSoftcam.camconfig.value or self.camdirold != config.plugins.AltSoftcam.camdir.value:
-			self.session.openWithCallback(self.updateConfig, MessageBox, (_("Are you sure you want to save this configuration?\n\n")))
-		elif not os.path.isdir(self.camconfigold) or not os.path.isdir(self.camdirold):
+		if self.camconfigold != config.plugins.AltSoftcam.camconfig.value \
+			or self.camdirold != config.plugins.AltSoftcam.camdir.value:
+			self.session.openWithCallback(self.updateConfig, MessageBox,
+				(_("Are you sure you want to save this configuration?\n\n")))
+		elif not path.isdir(self.camconfigold) or not path.isdir(self.camdirold):
 			self.updateConfig(True)
 		else:
 			self.close()
@@ -334,29 +370,34 @@ class ConfigEdit(Screen, ConfigListScreen):
 		if ret == True:
 			global AltSoftcamConfigError
 			msg = [ ]
-			if not os.path.isdir(config.plugins.AltSoftcam.camconfig.value):
+			if not path.isdir(config.plugins.AltSoftcam.camconfig.value):
 				msg.append("%s " % config.plugins.AltSoftcam.camconfig.value)
-			if not os.path.isdir(config.plugins.AltSoftcam.camdir.value):
+			if not path.isdir(config.plugins.AltSoftcam.camdir.value):
 				msg.append("%s " % config.plugins.AltSoftcam.camdir.value)
 			if msg == [ ]:
 				if config.plugins.AltSoftcam.camconfig.value[-1] == "/":
-					config.plugins.AltSoftcam.camconfig.value = config.plugins.AltSoftcam.camconfig.value[:-1]
+					config.plugins.AltSoftcam.camconfig.value = \
+						config.plugins.AltSoftcam.camconfig.value[:-1]
 				if config.plugins.AltSoftcam.camdir.value[-1] == "/":
-					config.plugins.AltSoftcam.camdir.value = config.plugins.AltSoftcam.camdir.value[:-1]
+					config.plugins.AltSoftcam.camdir.value = \
+						config.plugins.AltSoftcam.camdir.value[:-1]
 				config.plugins.AltSoftcam.camconfig.save()
 				config.plugins.AltSoftcam.camdir.save()
 				AltSoftcamConfigError = False
 				self.close()
 			else:
 				AltSoftcamConfigError = True
-				self.mbox = self.session.open(MessageBox, _("Directory %s does not exist!\nPlease set the correct directory path!") % msg, MessageBox.TYPE_INFO, timeout = 5 )
+				self.mbox = self.session.open(MessageBox,
+					_("Directory %s does not exist!\nPlease set the correct directorypath!")
+					% msg, MessageBox.TYPE_INFO, timeout = 5 )
 
 def main(session, **kwargs):
 	session.open(AltCamManager)
 
 def StartCam(reason, **kwargs):
 	global AltSoftcamConfigError
-	if AltSoftcamConfigError is False and config.plugins.AltSoftcam.actcam.value != "none":
+	if not AltSoftcamConfigError \
+		and config.plugins.AltSoftcam.actcam.value != "none":
 		if reason == 0: # Enigma start
 			try:
 				cmd = getcamcmd(config.plugins.AltSoftcam.actcam.value)
@@ -366,12 +407,19 @@ def StartCam(reason, **kwargs):
 				pass
 		elif reason == 1: # Enigma stop
 			try:
-				Console().ePopen("killall -9 %s" % config.plugins.AltSoftcam.actcam.value)
-				print "[Alternative SoftCam Manager] stopping ", config.plugins.AltSoftcam.actcam.value
+				Console().ePopen("killall -9 %s" %
+					config.plugins.AltSoftcam.actcam.value)
+				print "[Alternative SoftCam Manager] stopping ", \
+					config.plugins.AltSoftcam.actcam.value
 			except:
 				pass
 
 def Plugins(**kwargs):
 	return [
-	PluginDescriptor(name = _("Alternative SoftCam Manager"), description = _("Start, stop, restart SoftCams, change settings."), where = [ PluginDescriptor.WHERE_PLUGINMENU, PluginDescriptor.WHERE_EXTENSIONSMENU ], icon = "images/softcam.png", fnc = main),
-	PluginDescriptor(where = PluginDescriptor.WHERE_AUTOSTART, needsRestart = True, fnc = StartCam)]
+	PluginDescriptor(name = _("Alternative SoftCam Manager"),
+		description = _("Start, stop, restart SoftCams, change settings."),
+		where = [ PluginDescriptor.WHERE_PLUGINMENU,
+		PluginDescriptor.WHERE_EXTENSIONSMENU ],
+		icon = "images/softcam.png", fnc = main),
+	PluginDescriptor(where = PluginDescriptor.WHERE_AUTOSTART,
+		needsRestart = True, fnc = StartCam)]
